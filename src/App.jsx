6e23cc5bd3cf1a45f7297962a64a58406ce5fdfa
Eduardo Wronscki <51808558+eduwr@users.noreply.github.com>
2022-06-30 02:09:20 +0000

    hide connect button when wallet is connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,73 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
+  
+  /*
+  * Just a state variable we use to store our user's public wallet.
+  */
+  const [currentAccount, setCurrentAccount] = useState("");
 
+  /**
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+ 
+  /*
+  * This runs our function when the page loads.
+  */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  
   const wave = () => {
     
   }
@@ -13,7 +77,7 @@ export default function App() {
 
       <div className="dataContainer">
         <div className="header">
-        👋 Hey Folks!
+        👋 Hello!
         </div>
 
         <div className="bio">
@@ -22,6 +86,9 @@ export default function App() {
 
         <button className="waveButton" onClick={wave}>
           Wave at Me
+        </button>
+        <button className="waveButton" onClick={connectWallet}>
+         Connect Wallet
         </button>
       </div>
     </div>
